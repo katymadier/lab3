9003a43a5923f44cce6cc5447b525ce06417ff68
Katy Madier <me@katymadier.com>
2018-09-25 12:29:42 -0400

    extra credit

--- src/app/mock-heroes.ts
+++ src/app/mock-heroes.ts
@@ -2,14 +2,14 @@ import { Hero } from './hero';
 
 export const HEROES:
 Hero[] = [
-  { id: 11, name: 'Mr. Nice' },
-  { id: 12, name: 'Narco' },
-  { id: 13, name: 'Bombasto' },
-  { id: 14, name: 'Celeritas' },
-  { id: 15, name: 'Magneta' },
-  { id: 16, name: 'RubberMan' },
-  { id: 17, name: 'Dynama' },
-  { id: 18, name: 'Dr IQ' },
-  { id: 19, name: 'Magma' },
-  { id: 20, name: 'Tornado' }
+  { id: 11, name: 'Mr. Nice', pic:'../../assets/hero1.jpg'},
+  { id: 12, name: 'Narco', pic:'../../assets/hero2.jpg'},
+  { id: 13, name: 'Bombasto', pic:'../../assets/hero3.jpg' },
+  { id: 14, name: 'Celeritas', pic:'../../assets/hero4.jpg' },
+  { id: 15, name: 'Magneta', pic:'../../assets/hero5.jpg' },
+  { id: 16, name: 'RubberMan', pic: '../../assets/hero6.jpg' },
+  { id: 17, name: 'Dynama', pic:'../../assets/hero7.jpg' },
+  { id: 18, name: 'Dr IQ', pic:'../../assets/hero8.jpg' },
+  { id: 19, name: 'Magma' , pic:'../../assets/hero9.jpg'},
+  { id: 20, name: 'Tornado', pic:'../../assets/hero10.jpg' }
 ];
